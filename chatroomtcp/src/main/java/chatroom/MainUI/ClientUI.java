d778257a21e7173421a5be1d0521c57aae7ea3a0
Tomy-Ho <ho.duong.tomy@gmail.com>
2025-12-28 18:15:26 +0100

    initial setup for javafx

---
+++ chatroomtcp/src/main/java/chatroom/MainUI/ClientUI.java
@@ -0,0 +1,17 @@
+package chatroom.MainUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientUI extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
